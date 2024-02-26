1847e1d8d1af487f2f4434262e6090b1f404aeec
José Victor <victorlimaf55@gmail.com>
2024-02-26 19:28:07 -0300

    feature: criação das classes principais do jogo

---
+++ src/models/creatures/creature.py
@@ -0,0 +1,36 @@
+
+
+class Creature:
+    def __init__(self, hp: int, attack: int) -> None:
+        self.__hp = hp
+        self.__attack = attack
+
+    def getAttack(self) -> int:
+        return self.__attack
+
+    def setAttack(self, attack: int) -> None:
+        if attack <= 0:
+            return
+        self.__attack = attack
+    
+    def getHp(self) -> int:
+        return self.__hp
+
+    def setHp(self, hp: int) -> None:
+        if hp < 0:
+            return
+        self.__hp = hp
+
+    def setDamage(self, damage: int) -> None:
+        if (damage > self.getHp()):
+            damage = self.getHp()
+        self.setHp(self.getHp() - damage)
+    
+    def isDead(self) -> bool:
+        return self.getHp() == 0
+    
+    def isAlive(self) -> bool:
+        return not self.isDead()
+    
+def fight(c1: Creature, c2: Creature):
+    pass
---
+++ src/models/graph/graph.py
@@ -0,0 +1,34 @@
+from models.itens.item import Item
+
+class Vertex():
+    __value = 0
+
+    def __init__(self) -> None:
+        self.__value = Vertex.__value
+        Vertex.__value += 1
+        self.__itens = []
+
+    def getValue(self) -> int:
+        return self.__value
+    
+    def getVertexItens(self) -> list[Item]:
+        return self.__itens
+    
+    def addItem(self, item: Item) -> None:
+        self.__itens.append(item)
+
+class Graph():
+
+    def __init__(self) -> None:
+        self.__vertexs: dict[list] = {}
+
+    def addVertex(self, v:Vertex) -> None:
+        self.__vertexs[v.getValue()] = []
+
+    def addEdge(self, v:Vertex, w:Vertex) -> None:
+        self.__vertexs[v.getValue()].append(w)
+
+v1 = Vertex()
+v2 = Vertex()
+print(v1.getValue())
+print(v2.getValue())
