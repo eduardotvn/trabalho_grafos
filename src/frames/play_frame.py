b09da81c46fdbf4fc86ebf1c7bd96541e4fd5061
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-03-04 16:39:39 -0300

    Posições no mapa

--- src/frames/play_frame.py
+++ src/frames/play_frame.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+vertexes_on_map = [[100, 400], [160, 280], [312, 335], [285, 180], 
+                [460, 310], [472, 228], [590, 266], [492, 122], 
+                [415, 89], [632, 150], [619, 41], [709, 39],
+                [759, 177], [785, 310], [904, 161], [893, 37],
+                [1042, 126], [1149, 140], [1203, 37], [1110, 276]]
+
 class Play_Frame:
     def __init__(self, master, image_path, height, width):
         self.master = master
@@ -14,4 +20,4 @@ class Play_Frame:
         self.background_image.photo = photo
         self.background_image.pack()
 
-        
+
