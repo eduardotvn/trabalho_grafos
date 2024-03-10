39c4443031bc4b99fb23ff2770dea3f8b4769be3
José Victor <victorlimaf55@gmail.com>
2024-03-09 21:53:21 -0300

    Update search.py
    função de busca em largura gera agora percursos sem redundâncias.
    Adição de documentação no código.

--- src/models/search.py
+++ src/models/search.py
@@ -22,13 +22,14 @@ def deepSearch(graph: Graph, startVertex: Vertex) -> list[int]:
         vertex.setMark(Mark.BLACK)
         searchStack.pop()
 
-    searchStack = []
-    path: list[int] = []
+    searchStack = [] # pilha para armazenar o próximo vértice a ser explorado
+    path: list[int] = [] # a ordem do percuso dos vértices em profundidade 
 
     if graph.hasVertex(startVertex):
+        # todos os vértices devem começar como brancos
         for vertex in graph.getVertexs():
             vertex.setMark(Mark.WHITE)
-        graph.get(startVertex.getValue()).setMark(Mark.GRAY)
+        graph.get(startVertex.getValue()).setMark(Mark.GRAY) # o vértice inicial é marcado como cinza
         path.append(startVertex.getValue())
         visita(graph, startVertex, searchStack)
     return path
@@ -37,20 +38,21 @@ def  breadthFirstSearch(graph: Graph, startVertex: Vertex) -> list[int]:
     """
     Retorna uma lista de vértices a partir de uma busca em largura no grafo.
     """
-    path: list[int] = []
-    father: dict[int, int] = {}
+    path: list[int] = [] # a ordem do percuso dos vértices em profundidade
+    father: dict[int, int] = {} # armazena, para cada vértice, o vértice pelo qual este (chave) foi acessado primeiramente.
 
     for vertex in graph.getVertexs():
         vertex.setMark(Mark.WHITE)
         father[vertex.getValue()] = startVertex.getValue()
 
-    searchQueue: list[Vertex] = []
+    searchQueue: list[Vertex] = [] # fila para armazenar o próximo vértice a ser explorado
     searchQueue.insert(0, startVertex)
 
     startVertex.setMark(Mark.GRAY)
 
+    # enquando a fila tiver elementos a busca continua
     while len(searchQueue) > 0:
-        u = searchQueue.pop()
+        u = searchQueue.pop() # removendo o elemento mais antigo da fila
         path.append(u.getValue())
         for neighborn in graph.getAdjacentList(u):
             if neighborn.getMark() == Mark.WHITE:
@@ -59,19 +61,40 @@ def  breadthFirstSearch(graph: Graph, startVertex: Vertex) -> list[int]:
                 searchQueue.insert(0, neighborn)
         u.setMark(Mark.BLACK)
         
+        
+        nextValuePath = []
+        nextValueFather = startVertex.getValue()
         if len(searchQueue) > 0:
-            lastValue = path[-1]
-            while lastValue != startVertex.getValue():
-                lastValue = father[lastValue]
-                path.append(lastValue)
+            '''
+            Se a busca não chegou ao fim, então é gerado um caminho
+            do vértice iniciao até o pai do próximo vértice. 
+            '''
             nextValueFather = father[searchQueue[-1].getValue()]
             if nextValueFather != path[-1]:
-                nextValuePath = [nextValueFather]
+                nextValuePath.append(nextValueFather)
                 while nextValuePath[-1] != startVertex.getValue():
                     nextValuePath.append(father[nextValuePath[-1]])
-                nextValuePath.pop()
-                path.extend(nextValuePath[::-1])
-            
+            nextValuePath = nextValuePath[::-1]
+
+        lastValue = path[-1]
+        while lastValue != nextValueFather:
+            if lastValue in nextValuePath:
+                '''
+                Se o último vértice do percuso está contido no caminho do vértice
+                inicial até o pai do próximo vértice da busca, então podemos usar
+                esse caminho para chegar no próximo vértice.
+                '''
+                index = nextValuePath.index(lastValue)
+                for i in range(index + 1, len(nextValuePath)):
+                    path.append(nextValuePath[i])
+                break
+            '''
+            Caso contrário adiciona-se o pai do último vértice ao percuso.
+            Dessa forma ou chegaremos no pai do último vértice ou no vértice inicial,
+            e assim usaremos o caminho já calculado.
+            '''
+            lastValue = father[lastValue]
+            path.append(lastValue)
     return path
 
 def randomSearch(graph: Graph, vertex: Vertex):
